07bad722ecce1318b7c7bb608789c3b370e251e1
MultiversX <multiversx@MacBook.local>
2024-03-20 12:23:03 +0200

    configurations

--- src/config/config.devnet.ts
+++ src/config/config.devnet.ts
@@ -3,7 +3,7 @@ import { EnvironmentsEnum } from 'types';
 export * from './sharedConfig';
 
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqm6ad6xrsjvxlcdcffqe8w58trpec09ug9l5qde96pq';
+  'erd1qqqqqqqqqqqqqpgq6583tkxk5d2f27lyp4s82fkrp8knq403jcqsktkwt3';
 export const API_URL = 'https://devnet-template-api.multiversx.com';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
